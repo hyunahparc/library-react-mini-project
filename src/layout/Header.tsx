258979eb9c1b2b86444ea7e30c87bf2ad3f9c697
hyunahparc <hyunah.parc@gmail.com>
2026-02-07 04:28:35 +0900

    fix: align search logic with advanced search

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -11,44 +11,44 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 // fetch books based on search query
 const fetchBooks = async (query: string) => {
-  if (!query) return [];
+    if (!query) return [];
 
-  const response = await fetch(
-    `https://openlibrary.org/search.json?q=${encodeURIComponent(query)}`
-  );
-  if (!response.ok) throw new Error("Search failed");
-  return response.json();
+    const response = await fetch(
+      `https://openlibrary.org/search.json?q=${encodeURIComponent(query)}`
+    );
+    if (!response.ok) throw new Error("Search failed");
+    return response.json();
 };
 
 const Header = () => {
-  const [search, setSearch] = useState("");
-  const [open, setOpen] = useState(false);
-  const [debouncedSearch] = useDebounce(search, 300);
-  const navigate = useNavigate();
+    const [search, setSearch] = useState("");
+    const [open, setOpen] = useState(false);
+    const [debouncedSearch] = useDebounce(search, 300);
+    const navigate = useNavigate();
 
-  // fetch search suggestions
-  const { data } = useQuery({
-    queryKey: ['books', debouncedSearch],
-    queryFn: () => fetchBooks(debouncedSearch),
-    enabled: !!debouncedSearch,
-  });
+    // fetch search suggestions
+    const { data } = useQuery({
+      queryKey: ['books', debouncedSearch],
+      queryFn: () => fetchBooks(debouncedSearch),
+      enabled: !!debouncedSearch,
+    });
 
-  // close dropdown when clicking outside
-  const searchRef = useRef<HTMLFormElement | null>(null);
+    // close dropdown when clicking outside
+    const searchRef = useRef<HTMLFormElement | null>(null);
 
-  useEffect(() => {
-    const handleClickOutside = (e: MouseEvent) => {
-      if (
-        searchRef.current &&
-        !searchRef.current.contains(e.target as Node)
-      ) {
-        setOpen(false);
-      }
-    };
+    useEffect(() => {
+      const handleClickOutside = (e: MouseEvent) => {
+        if (
+          searchRef.current &&
+          !searchRef.current.contains(e.target as Node)
+        ) {
+          setOpen(false);
+        }
+      };
 
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
@@ -57,76 +57,76 @@ const Header = () => {
 
   // handle selecting a suggestion
   const handleSelect = (workKey: string) => {
-    setSearch("");
-    setOpen(false);
-    navigate(`/books/${workKey.replace("/works/", "")}`); // "/works/OLxxxxW"
+      setSearch("");
+      setOpen(false);
+      navigate(`/books/${workKey.replace("/works/", "")}`); // "/works/OLxxxxW"
   };
 
   // handle form submit
   const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    if (!debouncedSearch.trim()) return;
-    setOpen(false);
-    navigate(`/search?q=${debouncedSearch}`);
+      e.preventDefault();
+      if (!debouncedSearch.trim()) return;
+      setOpen(false);
+      navigate(`/search?title=${debouncedSearch}`);
   };
 
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="/">📚 Library</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="/advanced-search">Search</Nav.Link>
-          </Nav>
-          {/* search box */}
-          <Form className="d-flex position-relative" onSubmit={handleSubmit} ref={searchRef}>
-            <Form.Control
-              type="search"
-              placeholder="Search book"
-              className="me-2"
-              aria-label="Search"
-              value={search}
-              autoComplete="off"
-              style={{ zIndex: 1001 }}
-              onChange={(e) =>  {
-                setSearch(e.target.value);
-                setOpen(true);
-              }}
-            />
-            <Button variant="outline-success" onClick={handleSubmit}>Search</Button>
+        <Container fluid>
+            <Navbar.Brand href="/">📚 Library</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: '100px' }}
+                  navbarScroll
+                >
+                    <Nav.Link href="#action1">Home</Nav.Link>
+                    <Nav.Link href="/advanced-search">Search</Nav.Link>
+                </Nav>
+                {/* search box */}
+                <Form className="d-flex position-relative" onSubmit={handleSubmit} ref={searchRef}>
+                    <Form.Control
+                      type="search"
+                      placeholder="Search book"
+                      className="me-2"
+                      aria-label="Search"
+                      value={search}
+                      autoComplete="off"
+                      style={{ zIndex: 1001 }}
+                      onChange={(e) =>  {
+                        setSearch(e.target.value);
+                        setOpen(true);
+                      }}
+                    />
+                    <Button variant="outline-success" onClick={handleSubmit}>Search</Button>
 
-            {/* search suggestions list */}
-            {open &&  suggestions.length > 0 && (
-              <ListGroup
-                className="position-absolute w-100"
-                style={{ top: "100%", zIndex: 1000 }}
-              >
-                {suggestions.map((book: any) => (
-                  <ListGroup.Item
-                    key={book.key}
-                    action
-                    onMouseDown={() => handleSelect(book.key)}
-                  >
-                    <strong>{book.title}</strong>
-                    <div style={{ fontSize: "0.85rem", color: "#666" }}>
-                      {book.author_name?.[0] || "Unknown author"}
-                      {book.first_publish_year &&
-                        ` · ${book.first_publish_year}`}
-                    </div>
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-          )}
-          </Form>
-        </Navbar.Collapse>
-      </Container>
+                    {/* search suggestions list */}
+                    {open &&  suggestions.length > 0 && (
+                        <ListGroup
+                          className="position-absolute w-100"
+                          style={{ top: "100%", zIndex: 1000 }}
+                        >
+                            {suggestions.map((book: any) => (
+                                <ListGroup.Item
+                                  key={book.key}
+                                  action
+                                  onMouseDown={() => handleSelect(book.key)}
+                                >
+                                  <strong>{book.title}</strong>
+                                  <div style={{ fontSize: "0.85rem", color: "#666" }}>
+                                    {book.author_name?.[0] || "Unknown author"}
+                                    {book.first_publish_year &&
+                                      ` · ${book.first_publish_year}`}
+                                  </div>
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    )}
+                </Form>
+            </Navbar.Collapse>
+        </Container>
     </Navbar>
   );
 }
--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,15 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState, type SyntheticEvent } from "react";
 
 // Function to fetch search results from Open Library API
 const fetchSearchResults = async (
-    query: string,
+    params: Record<string, string>,
     page: number,
     limit: number
 ) => {
+    const queryParams = new URLSearchParams();
+
+    Object.entries(params).forEach(([key, value]) => {
+        if (value.trim()) {
+        queryParams.append(key, value);
+        }
+    });
+
+    queryParams.append("limit", limit.toString());
+    queryParams.append("offset", ((page - 1) * limit).toString());
+
     const response = await fetch(
-        `https://openlibrary.org/search.json?q=${encodeURIComponent(query)}&page=${page}&limit=${limit}`
+        `https://openlibrary.org/search.json?${queryParams.toString()}`
     );
 
     if (!response.ok) {
@@ -20,132 +31,168 @@ const fetchSearchResults = async (
 
 // Search Page Component
 export const SearchPage = () => {
-    
-    const navigate = useNavigate();
-    const [searchParams] = useSearchParams();
-    const query = searchParams.get("q") || "";
-    const [page, setPage] = useState(1);
-    const [limit, setLimit] = useState(18);
 
-    // Use React Query to fetch search results
-    const { data, isLoading, error } = useQuery({
-        queryKey: ['searchResults', query, page],
-        queryFn: () => fetchSearchResults(query, page, limit),
-        enabled: !!query,
+    const navigate = useNavigate();
+
+    const [searchParams] = useSearchParams();
+    // bring page number from url
+    const page = Number(searchParams.get("page") || 1);
+    const [limit] = useState(18);
+
+    useEffect(() => {
+        const hasParams = Array.from(searchParams.keys()).length > 0;
+
+        if (hasParams) {
+            setSubmittedFilters({
+            title: searchParams.get("title") || "",
+            author: searchParams.get("author") || "",
+            first_publish_year: searchParams.get("first_publish_year") || "",
+            subject: searchParams.get("subject") || "",
+            });
+        } else {
+            setSubmittedFilters(null);
+    }
+    }, [searchParams]);
+
+    const [filters, setFilters] = useState({
+        title: searchParams.get("title") || "",
+        author: searchParams.get("author") || "",
+        first_publish_year: searchParams.get("first_publish_year") || "",
+        subject: searchParams.get("subject") || "",
     });
 
-    if (!query) {
-         return <p>Please enter a search keyword.</p>;
-    }
+    const [submittedFilters, setSubmittedFilters] = useState<Record<string, string> | null>(null);
 
-    if (isLoading) return <p>Searching books…</p>;
-    if (error) return <p>Error loading search results.</p>;
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['searchResults', submittedFilters, page],
+        queryFn: () => fetchSearchResults(submittedFilters!, page, limit),
+        enabled: !!submittedFilters, // Only run query when filters are submitted
+    });
 
-    // Extract books from the fetched data
     const books = data?.docs || [];
 
-    // Calculate total pages for pagination
-    const total = data.numFound;
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, [e.target.name]: e.target.value });
+    };
+
+    // push search button
+    const handleSubmit = (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        const queryParams = new URLSearchParams();
+
+        Object.entries(filters).forEach(([key, value]) => {
+            if (value.trim()) {
+                queryParams.append(key, value);
+        }
+        });
+
+        queryParams.set("page", "1");
+
+        navigate(`/search?${queryParams.toString()}`);
+        setSubmittedFilters({...filters});
+    };
+
+    const goToPage = (nextPage: number) => {
+        const queryParams = new URLSearchParams(searchParams);
+        queryParams.set("page", nextPage.toString());
+
+        navigate(`/search?${queryParams.toString()}`);
+    };
+
+    // pagination
+    const total = data?.numFound || 0;
     const totalPages = Math.ceil(total / limit);
+
     const pageWindow = 5;
-    const startPage = Math.floor((page - 1) / pageWindow) * pageWindow + 1;
+    const startPage =
+    Math.floor((page - 1) / pageWindow) * pageWindow + 1;
     const endPage = Math.min(startPage + pageWindow - 1, totalPages);
+
 
     return (
         <div className="max-w-6xl mx-auto px-4 py-6">
-            <h2 className="text-2xl font-semibold mb-2">
-                🔍 Search results for <span className="text-blue-600">"{query}"</span>
-            </h2>
+        <h2 className="text-2xl font-semibold mb-2">
+            {/* 🔍 Search results for <span className="text-blue-600">"{title}"</span> */}
+        </h2>
 
-            <p className="text-gray-500">
-                {total} results found
-            </p>
+        <p className="text-gray-500">{total} results found</p>
 
-            {/* card list */}
-            <ul style={{ listStyle: "none", paddingLeft: 0 }} 
-            className="
-                grid gap-6
-                grid-cols-1
-                sm:grid-cols-2
-                lg:grid-cols-3
-            ">
-                {books.map((book: any) => (
-                    <li
-                        key={book.key}
-                        className="
-                        bg-white
-                        rounded-xl
-                        shadow-sm
-                        p-3
-                        border
-                        hover:shadow-md
-                        transition
-                        "
-                        onClick={() => navigate(`/books/${book.key.replace("/works/", "")}`)}
-                    >
-                    <div className="w-38 h-65 mb-4 mx-auto flex items-center justify-center">
-                        <img
-                        src={
-                            book.cover_i
-                            ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-                            : "https://openlibrary.org/images/icons/avatar_book-sm.png"
-                        }
-                        alt={book.title}
-                        className="w-full h-auto rounded"
-                        />
-                    </div>
-                    <h3 className="font-semibold text-lg mb-2">
-                    {book.title}
-                    </h3>
-
-                    <p className="text-lg text-gray-600 mb-2">
-                    {book.author_name
-                        ? book.author_name.join(", ")
-                        : "Unknown Author"}
-                    </p>
-
-                    {book.first_publish_year && (
-                    <p className="text-sm text-gray-400">
-                        First published: {book.first_publish_year}
-                    </p>
-                    )}
-                </li>
-                ))}
-            </ul>
-            {/* Pagination Controls */}
-            <div className="flex gap-2 mt-8 justify-center items-center flex-wrap">
-                <button
-                    disabled={page === 1}
-                    onClick={() => setPage((p) => p - 1)}
-                    className="px-4 py-2 rounded-lg bg-gray-200 text-gray-700 font-medium hover:bg-gray-300 disabled:opacity-50 disabled:cursor-not-allowed transition"
-                >
-                    ◀ Prev
-                </button>
-
-                {Array.from(
-                    { length: endPage - startPage + 1 },
-                    (_, i) => startPage + i
-                ).map((p) => (
-                    <button
-                        key={p}
-                        onClick={() => setPage(p)}
-                        style={{
-                        fontWeight: p === page ? "bold" : "normal",
-                        }}
-                    >
-                        {p}
-                    </button>
-                ))}
-
-                <button
-                    disabled={page === totalPages}
-                    onClick={() => setPage((p) => p + 1)}
-                    className="px-4 py-2 rounded-lg bg-gray-200 text-gray-700 font-medium hover:bg-gray-300 disabled:opacity-50 disabled:cursor-not-allowed transition"
-                >
-                    Next ▶
-                </button>
+        {/* no result message */}
+        {!isLoading && total === 0 && (
+            <div className="mt-12 text-center text-black-500">
+            <p className="text-2xl font-medium">No result</p>
+            <p className="mt-2 text-sm">Please try again with other keywords!</p>
             </div>
+        )}
+
+        {/* card list */}
+        <ul
+            style={{ listStyle: "none", paddingLeft: 0 }}
+            className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
+        >
+            {books.map((book: any) => (
+            <li
+                key={book.key}
+                className="bg-white rounded-xl shadow-sm p-3 border hover:shadow-md transition"
+                onClick={() =>
+                navigate(`/books/${book.key.replace("/works/", "")}`)
+                }
+            >
+                <div className="w-38 h-65 mb-4 mx-auto flex items-center justify-center">
+                <img
+                    src={
+                    book.cover_i
+                        ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+                        : "https://openlibrary.org/images/icons/avatar_book-sm.png"
+                    }
+                    alt={book.title}
+                    className="w-full h-auto rounded"
+                />
+                </div>
+                <h3 className="font-semibold text-lg mb-2">{book.title}</h3>
+                <p className="text-lg text-gray-600 mb-2">
+                {book.author_name ? book.author_name.join(", ") : "Unknown Author"}
+                </p>
+                {book.first_publish_year && (
+                <p className="text-sm text-gray-400">
+                    First published: {book.first_publish_year}
+                </p>
+                )}
+            </li>
+            ))}
+        </ul>
+
+        {/* Pagination Controls */}
+        {books.length > 0 && (
+            <div className="flex gap-2 mt-8 justify-center items-center flex-wrap">
+            <button
+                disabled={page === 1}
+                onClick={() => goToPage(page - 1)}
+                className="px-4 py-2 rounded-lg bg-gray-200 text-gray-700 font-medium hover:bg-gray-300 disabled:opacity-50 disabled:cursor-not-allowed transition"
+            >
+                ◀ Prev
+            </button>
+
+            {Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i).map((p) => (
+                <button
+                key={p}
+                onClick={() => goToPage(p)}
+                style={{ fontWeight: p === page ? "bold" : "normal" }}
+                >
+                {p}
+                </button>
+            ))}
+
+            <button
+                disabled={page === totalPages}
+                onClick={() => goToPage(page + 1)}
+                className="px-4 py-2 rounded-lg bg-gray-200 text-gray-700 font-medium hover:bg-gray-300 disabled:opacity-50 disabled:cursor-not-allowed transition"
+            >
+                Next ▶
+            </button>
+            </div>
+        )}
         </div>
     );
-}
-
+};
