7b5137fef047b73b3e80e74291e147610031ec30
hyunahparc <hyunah.parc@gmail.com>
2026-02-09 18:37:50 +0100

    style: fix footer at bottom

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,11 +4,13 @@ import Header from "./Header";
 
 const Layout = () => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <Outlet />
+      <div className="flex-1">
+        <Outlet />
+      </div>
       <Footer />
-    </>
+    </div>
   )
 }
 
